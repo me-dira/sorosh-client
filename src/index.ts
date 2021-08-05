d43aa0e008ba3ab5f3a19fba04abc3943cf42689
me-dira <mehdirahimi.cpp@gmail.com>
2021-08-05 18:34:36 +0430

    fix: comments fixed and changed

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import "reflect-metadata";
 
+/**
+ * Main namespace of soroush comments
+ */
 export namespace Soroush {
-    /**
-     * @name Soroush Client object
-     * @author <Mehdi Rahimi mediraworkm@gmail.com>
-     */
-    export class SoroushClient implements Client {
-        constructor() {
 
-        }
+    export class SoroushClient implements Client {
+        /**
+         * @name Soroush Client object
+         * @author <Mehdi Rahimi mediraworkm@gmail.com>
+         * @param {token} Your soroush bot token
+         * @constructor
+         */
+        constructor(private readonly token: string) { }
 
         on(type: SoroushDataTypes, callback: ClientMethodCallback) { }
         message(message: string, callback: ClientMethodCallback) { }
